97b7ae0293f8d55f64337d44a9770d773f5106b0
Erika <erikajaquez@newschool.edu>
2017-10-31 19:01:16 -0400

    !
    !

--- creative_computing/clock/clock.js
+++ creative_computing/clock/clock.js
@@ -9,22 +9,20 @@ function updateTime() {
 
 	var clockHolder = document.getElementById("clock")
 	var now = new Date()
-	var seconds = now.getMinutes()
+	var minutes = now.getMinutes()
 	now = now.toTimeString()
+
+	
 
 
 		var circle1 = document.getElementById("circle1") 
-		var minutes = now.getMinutes()
 		if (minutes == 0) {
 			circle1.style.color = "#DC9896"
 		}
 
 
 
-		var circle2 = document.getElementById("circle2") 
-
-		var minutes = now.getMinutes()
-
+		var circle2 = document.getElementById("circle2")
 		if (minutes == 1) {
 			circle2.style.color = "#DC9492"
 		}
@@ -32,9 +30,6 @@ function updateTime() {
 
 
 		var circle3 = document.getElementById("circle3") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 2) {
 			circle3.style.color = "#DC9290"
 		}
@@ -42,135 +37,90 @@ function updateTime() {
 
 
 		var circle4 = document.getElementById("circle4") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 3) {
 			circle4.style.color = "#DC918E"
 		}
 
 
 		var circle5 = document.getElementById("circle5") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 4) {
 			circle5.style.color = "#DC908C" 
 		}
 
 
 		var circle6 = document.getElementById("circle6") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 5) {
 			circle6.style.color = "#DC908B"
 		}
 
 
 		var circle7 = document.getElementById("circle7") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 6) {
 			circle7.style.color = "#DC8F89"
 		}
 
 
 		var circle8 = document.getElementById("circle8") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 7) {
 			circle8.style.color = "#DC8D87"
 		}
 
 
 		var circle9 = document.getElementById("circle9") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 8) {
 			circle9.style.color = "#DC8B84"
 		}
 
 
 		var circle10 = document.getElementById("circle10") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 9) {
 			circle10.style.color = "#DC8981"
 		}
 
 
 		var circle11 = document.getElementById("circle11") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 10) {
 			circle11.style.color = "#DC887F"
 		}
 
 
-			var circle12 = document.getElementById("circle12") 
-
-		var minutes = now.getMinutes()
-
+		var circle12 = document.getElementById("circle12") 
 		if (minutes == 11) {
 			circle12.style.color = "#DC867D"
 		}
 
 
-			var circle13 = document.getElementById("circle13") 
-
-		var minutes = now.getMinutes()
-
+		var circle13 = document.getElementById("circle13") 
 		if (minutes == 12) {
 			circle13.style.color = "#DC847A"
 		}
 
 
 		var circle14 = document.getElementById("circle14") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 13) {
 			circle14.style.color = "#DC8277"
 		}
 
 
 		var circle15 = document.getElementById("circle15") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 14) {
 			circle15.style.color = "#DC8075"
 		}
 
 
 		var circle16 = document.getElementById("circle16") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 15) {
 			circle16.style.color = "#DC7B6F"
 		}
 
 
 		var circle17 = document.getElementById("circle17") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 16) {
 			circle17.style.color = "#DC776C"
 		}
 
 
 		var circle18 = document.getElementsById("circle18") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 17) {
 			circle18.style.color = "#DC7267"
 		}
@@ -178,36 +128,24 @@ function updateTime() {
 
 
 		var circle19 = document.getElementById("circle19") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 18) {
 			circle19.style.color = "#DC6E64"
 		}
 
 
 		var circle20 = document.getElementById("circle20") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 19) {
 			circle20.style.color = "#DC6B62"
 		}
 
 
 		var circle21 = document.getElementById("circle21") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 20) {
 			circle21.style.color = "#DC685F"
 		}
 
 
 		var circle22 = document.getElementById("circle22") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 21) {
 			circle22.style.color = "#DC655D"
 		}
@@ -215,18 +153,12 @@ function updateTime() {
 
 
 		var circle23 = document.getElementById("circle23") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 22) {
 			circle23.style.color = "#DC625A"
 		}
 
 
 		var circle24 = document.getElementById("circle24") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 23) {
 			circle24.style.color = "#DC5D56"
 		}
@@ -234,9 +166,6 @@ function updateTime() {
 
 
 		var circle25 = document.getElementById("circle25") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 24) {
 			circle25.style.color = "#DC5953"
 		}
@@ -244,9 +173,6 @@ function updateTime() {
 
 
 		var circle26 = document.getElementById("circle26") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 25) {
 			circle26.style.color = "#DC5750"
 		}
@@ -255,9 +181,6 @@ function updateTime() {
 
 
 		var circle27 = document.getElementById("circle27") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 26) {
 			circle27.style.color = "#DC524C"
 		}
@@ -265,9 +188,6 @@ function updateTime() {
 
 
 		var circle28 = document.getElementById("circle28") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 27) {
 			circle28.style.color = "#DC4F49"
 
@@ -277,9 +197,6 @@ function updateTime() {
 
 
 		var circle29 = document.getElementById("circle29") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 28) {
 			circle29.style.color = "#DC4B45"
 		}
@@ -287,9 +204,6 @@ function updateTime() {
 
 
 		var circle30 = document.getElementById("circle30") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 29) {
 			circle30.style.color = "#DC4741"
 		}
@@ -297,9 +211,6 @@ function updateTime() {
 
 
 		var circle31 = document.getElementById("circle31") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 30) {
 			circle31.style.color = "#DC443D"
 		}
@@ -312,9 +223,6 @@ function updateTime() {
 
 
 		var circle33 = document.getElementById("circle33") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 1) {
 			circle32.style.color = "#DC3C37"
 		}
@@ -322,9 +230,6 @@ function updateTime() {
 
 
 		var circle34 = document.getElementById("circle34") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 33) {
 			circle34.style.color = "#DC3934"
 		}
@@ -332,135 +237,90 @@ function updateTime() {
 
 
 		var circle35 = document.getElementById("circle35") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 34) {
 			circle35.style.color = "#DC3631"
 		}
 
 
 		var circle36 = document.getElementById("circle36") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 35) {
 			circle36.style.color = "#DC342F" 
 		}
 
 
 		var circle37 = document.getElementById("circle37") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 36) {
 			circle37.style.color = "#DC312C"
 		}
 
 
 		var circle38 = document.getElementById("circle38") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 37) {
 			circle38.style.color = "#DC2D29"
 		}
 
 
 		var circle39 = document.getElementById("circle39") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 38) {
 			circle39.style.color = "#DC2A29"
 		}
 
 
 		var circle40 = document.getElementById("circle40") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 39) {
 			circle40.style.color = "#DC272B"
 		}
 
 
 		var circle41 = document.getElementById("circle41") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 40) {
 			circle41.style.color = "#DC2430"
 		}
 
 
 		var circle42 = document.getElementById("circle42") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 41) {
 			circle42.style.color = "#DC2134"
 		}
 
 
 		var circle43 = document.getElementById("circle43") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 42) {
 			circle43.style.color = "#DC1F3B"
 		}
 
 
 		var circle44 = document.getElementById("circle44") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 43) {
 			circle44.style.color = "#D71F3B"
 		}
 
 
 		var circle45 = document.getElementById("circle45") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 44) {
 			circle45.style.color = "#D31F3B"
 		}
 
 
 		var circle46 = document.getElementById("circle46") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 45) {
 			circle46.style.color = "#CF1F3B"
 		}
 
 
 		var circle47 = document.getElementById("circle47") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 46) {
 			circle47.style.color = "#C91F3B"
 		}
 
 
 		var circle48 = document.getElementById("circle48") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 47) {
 			circle48.style.color = "#C61F3B"
 		}
 
 
 		var circle49 = document.getElementsById("circle49") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 48) {
 			circle49.style.color = "#9E1931"
 		}
@@ -468,36 +328,24 @@ function updateTime() {
 
 
 		var circle50 = document.getElementById("circle50") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 49) {
 			circle50.style.color = "#9C1931"
 		}
 
 
 		var circle51 = document.getElementById("circle51") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 50) {
 			circle51.style.color = "#991931"
 		}
 
 
 		var circle52 = document.getElementById("circle52") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 51) {
 			circle52.style.color = "#951931"
 		}
 
 
 		var circle53 = document.getElementById("circle53") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 52) {
 			circle53.style.color = "#901931"
 		}
@@ -505,18 +353,12 @@ function updateTime() {
 
 
 		var circle54 = document.getElementById("circle54") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 53) {
 			circle54.style.color = "#8D1931"
 		}
 
 
 		var circle55 = document.getElementById("circle55") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 54) {
 			circle55.style.color = "#891931"
 		}
@@ -524,9 +366,6 @@ function updateTime() {
 
 
 		var circle56 = document.getElementById("circle56") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 55) {
 			circle56.style.color = "#861931"
 		}
@@ -534,9 +373,6 @@ function updateTime() {
 
 
 		var circle57 = document.getElementById("circle57") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 56) {
 			circle57.style.color = "#821931"
 		}
@@ -545,9 +381,6 @@ function updateTime() {
 
 
 		var circle58 = document.getElementById("circle58") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 57) {
 			circle58.style.color = "#7F1931"
 		}
@@ -555,9 +388,6 @@ function updateTime() {
 
 
 		var circle59 = document.getElementById("circle59") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 58) {
 			circle59.style.color = "#7C1931"
 
@@ -567,16 +397,15 @@ function updateTime() {
 
 
 		var circle60 = document.getElementById("circle60") 
-
-		var minutes = now.getMinutes()
-
 		if (minutes == 59) {
 			circle60.style.color = "#781931"
 		}
 
 
-		setInterval (update, 1000) 
+		 
 	}
+
+	setInterval (update, 1000) 
 
 
 
